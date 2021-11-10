97aff4524af1aa299422c0aa71d5c1187b1097e4
santibh <santiagobarreirohermida@gmail.com>
2021-11-10 22:31:24 +0100

    Añadido botón Crear nuevo alumno y arreglado link de Crear nuevo profesor

--- p1-client/components/Students/StudentList.js
+++ p1-client/components/Students/StudentList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getStudents } from "../../api/student";
+import { Button } from "semantic-ui-react";
 
 const StudentList = () => {
   const [students, setStudents] = useState();
@@ -13,13 +14,26 @@ const StudentList = () => {
   }, []);
 
   return (
-    <div style={{margin: "10%"}}>
+    <div style={{ margin: "10%" }}>
       {students !== undefined &&
         students.map((x) => (
           <p>
             <a href={x.id}>{x.nombre}</a>
+            <Button
+              class="ui negative basic button"
+              style={{ marginLeft: "1em" }}
+            >
+              Eliminar
+            </Button>
           </p>
         ))}
+      <div style={{ textAlign: "right" }}>
+        <Button class="ui primary button">
+          <a href="/students/registro_alumnos">
+            <p>Añadir nuevo alumno</p>
+          </a>
+        </Button>
+      </div>
     </div>
   );
 };
