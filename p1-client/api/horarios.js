6085257f7b38b74ca5758173d0b18d07996f618f
manuel.couto1 <manuel.couto1@udc.es>
2021-11-19 11:49:33 +0100

    Cambios tabla

--- p1-client/api/horarios.js
+++ p1-client/api/horarios.js
@@ -1,25 +1,25 @@
-import { BASE_PATH_JSON_SERVER } from "../utils/constants";
+import { BASE_PATH_JSON_SERVER, BASE_PATH } from '../utils/constants'
 
 export async function getAllHorarios() {
   try {
-    const url = `${BASE_PATH_JSON_SERVER}/horarios`;
-    
-    const response = await fetch(url, null);
-    const result = await response.json();
-    return result;
+    const url = `${BASE_PATH_JSON_SERVER}/horarios`
+
+    const response = await fetch(url, null)
+    const result = await response.json()
+    return result
   } catch (error) {
-    return null;
+    return null
   }
 }
 
 export async function getAllEvents() {
-    try {
-      const url = `${BASE_PATH_JSON_SERVER}/events`;
-      
-      const response = await fetch(url, null);
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      return null;
-    }
+  try {
+    const url = `${BASE_PATH_JSON_SERVER}/events`
+
+    const response = await fetch(url, null)
+    const result = await response.json()
+    return result
+  } catch (error) {
+    return null
   }
+}
