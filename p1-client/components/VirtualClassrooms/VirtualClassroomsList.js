89954cbdce2df31b032c2458c28662d813599f15
santibh <santiagobarreirohermida@gmail.com>
2021-12-01 18:21:57 +0100

    Integrado el sistema de grupos con el de mensajes

--- p1-client/components/VirtualClassrooms/VirtualClassroomsList.js
+++ p1-client/components/VirtualClassrooms/VirtualClassroomsList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getVirtualClassrooms } from "../../api/virtualClassroom";
+import { getMyGroups, getGroups, getTeacherGroups } from "../../api/group";
 
 const VirtualClassroomsList = () => {
   const [virtualClassrooms, setVirtualClassrooms] = useState();
@@ -8,7 +8,12 @@ const VirtualClassroomsList = () => {
   useEffect(() => {
     // Cargamos las aulas virtuales
     async function fetchMyAPI() {
-      const response = await getVirtualClassrooms();
+      const role = sessionStorage.getItem("user_role").toLowerCase();
+      const response =
+        role === "secretario"
+          ? await getGroups() : role === "profesor" ? await getTeacherGroups()
+          : await getMyGroups();
+      console.log(response) 
       setVirtualClassrooms(response);
     }
     fetchMyAPI();
@@ -18,16 +23,14 @@ const VirtualClassroomsList = () => {
     <div style={{ padding: "2%" }}>
       <h3>Lista de aulas virtuales</h3>
       <p>
-        {virtualClassrooms !== undefined && virtualClassrooms.length === 0 && (
+        {virtualClassrooms && virtualClassrooms !== undefined && virtualClassrooms.length === 0 && (
           <p>No hay disponible ningún aula virtual</p>
         )}
-        {virtualClassrooms !== undefined &&
+        {virtualClassrooms && virtualClassrooms !== undefined &&
           virtualClassrooms.map((x) => {
             return (
               <div style={{ margin: "5%", marginTop: "0" }}>
-                <a
-                  href={x.id}
-                >{`${x.grupo.nombre}`}</a>
+                <a href={x._id}>{`${x.nombre}`}</a>
               </div>
             );
           })}
